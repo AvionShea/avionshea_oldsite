f5aefb44e6120f634d41f46a42489d62e176a1ef
AvionShea <avionsheadesigns@gmail.com>
2024-02-17 01:06:35 -0500

    changes to wording in index and hero components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Greetings and Salutations! I'm <span className='text-[#915eff]'>Avion!</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I utilize my skills to produce great websites <br className='sm:block hidden' /> and even better web applications.</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>Foodie. Curious. Software Engineer. Lover of the Cosmos.</p>
         </div>
       </div>
 
